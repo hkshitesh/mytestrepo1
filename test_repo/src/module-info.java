a53aa137fc9bdc05e376e8876cab9f3472d1425a
Hitesh Kumar Sharma <hiteshupes@gmail.com>
2022-09-14 23:49:32 +0530

    first commit

---
+++ test_repo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hksharma
+ *
+ */
+module test_repo {
+}
---
+++ test_repo/src/test_repo/myclass.java
@@ -0,0 +1,11 @@
+package test_repo;
+
+public class myclass {
+
+	public static void main(String[] args) {
+
+System.out.println("hello");
+
+	}
+
+}
